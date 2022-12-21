ced4188ce9e5a96ff82694c3584924dbef5e5d0f
anngreen <anngreen@qq.com>
2022-12-21 16:36:06 +0800

    docs props

--- Vue/Vue3/code/vue3-javascript-demo/src/router/routes/ComponentsInDepth.js
+++ Vue/Vue3/code/vue3-javascript-demo/src/router/routes/ComponentsInDepth.js
@@ -4,5 +4,21 @@ const ComponentsInDepth = [
     name: "ComponentsInDepth",
     component: () => import("@/views/ComponentsInDepth/ComponentsInDepth.vue"),
   },
+  /**
+   * Registration
+   */
+  {
+    path: "/ComponentsInDepth/Registration/Registration",
+    name: "ComponentsInDepth-Registration-Registration",
+    component: () => import("@/views/ComponentsInDepth/Registration/Registration.vue"),
+  },
+  /**
+   * Props
+   */
+  {
+    path: "/ComponentsInDepth/Props/Props",
+    name: "ComponentsInDepth-Props-Props",
+    component: () => import("@/views/Props/Props.vue"),
+  },
 ];
 export default ComponentsInDepth;
