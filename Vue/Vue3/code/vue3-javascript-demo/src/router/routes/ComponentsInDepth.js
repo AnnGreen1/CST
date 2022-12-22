73b763c2bce58046e0e9ecb781266240a540ed39
anngreen <anngreen@qq.com>
2022-12-22 15:03:26 +0800

    props

--- Vue/Vue3/code/vue3-javascript-demo/src/router/routes/ComponentsInDepth.js
+++ Vue/Vue3/code/vue3-javascript-demo/src/router/routes/ComponentsInDepth.js
@@ -12,13 +12,18 @@ const ComponentsInDepth = [
     name: "ComponentsInDepth-Registration-Registration",
     component: () => import("@/views/ComponentsInDepth/Registration/Registration.vue"),
   },
+  {
+    path: "/ComponentsInDepth/Registration/RegistrationWithoutSetup",
+    name: "ComponentsInDepth-Registration-RegistrationWithoutSetup",
+    component: () => import("@/views/ComponentsInDepth/Registration/RegistrationWithoutSetup.vue"),
+  },
   /**
    * Props
    */
   {
     path: "/ComponentsInDepth/Props/Props",
     name: "ComponentsInDepth-Props-Props",
-    component: () => import("@/views/Props/Props.vue"),
+    component: () => import("@/views/ComponentsInDepth/Props/Props.vue"),
   },
   /**
    * Events
