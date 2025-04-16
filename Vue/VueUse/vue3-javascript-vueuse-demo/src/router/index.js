135f440084aab1c5eecf49c9b44b351e1ced13db
anngreen <anngreen@qq.com>
2025-04-16 22:24:17 +0800

    vueuse

---
+++ Vue/VueUse/vue3-javascript-vueuse-demo/src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+    // history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: '/home',
+            name: 'home',
+            component: HomeView
+        },
+    ]
+})
+
+export default router
